c024ab444e14b79b8ac241708ba3104599ecf710
Quan Truong <quantt86@gmail.com>
2020-08-06 11:47:32 +0700

    Update find-non-av-installed-computers.sh

--- find-non-av-installed-computers.sh
+++ find-non-av-installed-computers.sh
@@ -7,9 +7,9 @@ fi
 #scan port 445 to detect windows computers, ecluded printers
 masscan $1 -p135,137,139,445,3389 --excludefile exclude.txt| cut -d' ' -f6 > "445-$2.txt"
 #scan port 64000 to detect computers installed trend v14
-masscan $1 -p64000 | cut -d' ' -f6 > "64000-$2.txt"
+masscan $1 -p64000,17774 | cut -d' ' -f6 > "64000-$2.txt"
 #scan port 17774 to detect computers installed trend v12
-masscan $1 -p17774 | cut -d' ' -f6 >> "64000-$2.txt"
+#masscan $1 -p17774 | cut -d' ' -f6 >> "64000-$2.txt"
 sort -u "445-$2.txt" -o "445-$2.txt"
 sort -u "64000-$2.txt" -o "64000-$2.txt"
 echo "These computer has not installed trend:"
