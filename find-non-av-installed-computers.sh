8bf86103568130510ba126d43920148f7bdbabc8
Quan Truong <quantt86@gmail.com>
2020-08-06 11:45:52 +0700

    Add files via upload

--- find-non-av-installed-computers.sh
+++ find-non-av-installed-computers.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] ; then
 	echo "e.g: $0 10.68.5.0/24"
 fi
 #scan port 445 to detect windows computers, ecluded printers
-masscan $1 -p445 --excludefile exclude.txt| cut -d' ' -f6 > "445-$2.txt"
+masscan $1 -p135,137,139,445,3389 --excludefile exclude.txt| cut -d' ' -f6 > "445-$2.txt"
 #scan port 64000 to detect computers installed trend v14
 masscan $1 -p64000 | cut -d' ' -f6 > "64000-$2.txt"
 #scan port 17774 to detect computers installed trend v12
