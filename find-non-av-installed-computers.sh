745f55770e41d69f8a1a6684ec313d8eba2ff9a2
Quan Truong <quantt86@gmail.com>
2020-08-06 11:15:23 +0700

    Add files via upload

--- find-non-av-installed-computers.sh
+++ find-non-av-installed-computers.sh
@@ -4,8 +4,13 @@ if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] ; then
 	echo "Usage: $0 network_range"
 	echo "e.g: $0 10.68.5.0/24"
 fi
-#scan port 445 to detect windows computers
-masscan -p445 --rate 20000 $1 | cut -d' ' -f6 > "445.txt"
-masscan -p64000 --rate 20000 $1 | cut -d' ' -f6 > "64000.txt"
+#scan port 445 to detect windows computers, ecluded printers
+masscan $1 -p445 --excludefile exclude.txt| cut -d' ' -f6 > "445-$2.txt"
+#scan port 64000 to detect computers installed trend v14
+masscan $1 -p64000 | cut -d' ' -f6 > "64000-$2.txt"
+#scan port 17774 to detect computers installed trend v12
+masscan $1 -p17774 | cut -d' ' -f6 >> "64000-$2.txt"
+sort -u "445-$2.txt" -o "445-$2.txt"
+sort -u "64000-$2.txt" -o "64000-$2.txt"
 echo "These computer has not installed trend:"
-fgrep -v -f 64000.txt 445.txt
+fgrep -v -f "64000-$2.txt" "445-$2.txt"
